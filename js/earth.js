13f7f22a1923c6e5f785c4a36ffeb96ba86ed3c4
Victor Shcherb <victor.shcherb@gmail.com>
2022-08-27 16:46:09 +0200

    Add earth html

--- js/earth.js
+++ js/earth.js
@@ -4,8 +4,14 @@ const h = 1.01; // earth skew
 const EarthDelta = Math.PI / 180 * 5; // up to 85 (Mercatoor projection)
 
 const CONFIG = {
+    loadTexture: true,
     updateBuffer: false,
+    textureZoom: 0,
+    // defaultURL: 'https://tile.osmand.net',
+    defaultURL: 'https://tile.openstreetmap.org',
+    animationSpeed: 0.001,
     eyeDist: 0,
+    eyePosition : -12,
     drawMode: 'TRIANGLES',
     cubeRotation: 0.0,
     zoom: 3,
@@ -39,15 +45,18 @@ function main() {
     const vsSource = `
     attribute vec4 aVertexPosition;
     attribute vec4 aVertexColor;
+    attribute vec2 aTextureCoord;
 
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
 
     varying lowp vec4 vColor;
+    varying highp vec2 vTextureCoord;
 
     void main(void) {
       gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
       vColor = aVertexColor;
+      vTextureCoord = aTextureCoord;
     }
   `;
 
@@ -55,9 +64,12 @@ function main() {
 
     const fsSource = `
     varying lowp vec4 vColor;
+    varying highp vec2 vTextureCoord;
+    uniform sampler2D uSampler;
 
     void main(void) {
-      gl_FragColor = vColor;
+       //gl_FragColor = vColor; // random color 
+       gl_FragColor = texture2D(uSampler, vTextureCoord); // tiles
     }
   `;
 
@@ -74,31 +86,45 @@ function main() {
         attribLocations: {
             vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
             vertexColor: gl.getAttribLocation(shaderProgram, "aVertexColor"),
+            textureCoord: gl.getAttribLocation(shaderProgram, "aTextureCoord"),
         },
         uniformLocations: {
-            projectionMatrix: gl.getUniformLocation(
-                shaderProgram,
-                "uProjectionMatrix"
-            ),
+            projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
             modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+            uSampler: gl.getUniformLocation(shaderProgram, "uSampler"),
         },
     };
-
+    addListeners();
     // Here's where we call the routine that builds all the
     // objects we'll be drawing.
     var buffers = initBuffers(gl);
-    addListeners();
+    // https://tile.openstreetmap.org/3/4/2.png
+    // Browsers copy pixels from the loaded image in top-to-bottom order —
+    // from the top-left corner; but WebGL wants the pixels in bottom-to-top
+    // order — starting from the bottom-left corner. So in order to prevent
+    // the resulting image texture from having the wrong orientation when
+    // rendered, we need to make the following call, to cause the pixels to
+    // be flipped into the bottom-to-top order that WebGL expects.
+
+    
     var then = 0;
     // Draw the scene repeatedly
+    var texture;
     function render(now) {
         if (CONFIG.updateBuffer) {
             buffers = initBuffers(gl);
             CONFIG.updateBuffer = false;
         }
-        now *= 0.001; // convert to seconds
+        if (CONFIG.loadTexture) {
+            texture = loadTexture(gl, CONFIG.defaultURL, CONFIG.textureZoom);
+            // See jameshfisher.com/2020/10/22/why-is-my-webgl-texture-upside-down
+            // gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+            CONFIG.loadTexture = false;
+        }
+        now *= CONFIG.animationSpeed; // convert to seconds
         const deltaTime = now - then;
         then = now;
-        drawScene(gl, programInfo, buffers, deltaTime);
+        drawScene(gl, programInfo, buffers, deltaTime, texture);
         requestAnimationFrame(render);
     }
     requestAnimationFrame(render);
@@ -111,31 +137,21 @@ function initBuffers(gl) {
     // Now set up the colors for the faces. We'll use solid colors
     // for each face.
     const sides = CONFIG.sides();
-    var faceColors = [];
+    const faceColors = [];
     for (var cind = 0; cind < sides * 2; cind++) {
         faceColors.push(hsv2rgb((360 / (sides * 2)) * cind, 0.9, 0.9, 1));
     }
-
-    // Convert the array of colors into a table for all the vertices.
-    var colors = [];
-
-    // Create a buffer for the cube's vertex positions.
-    const positionBuffer = gl.createBuffer();
-    // Select the positionBuffer as the one to apply buffer
-    // operations to from here out.
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
     var ind = 0;
     var vertCount = 0;
     // Now create an array of positions for the cube.
-    var positions = [];
-    // This array defines each face as two triangles, using the
-    // indices into the vertex array to specify each triangle's position.
-    var indices = []
+    let colors = [];
+    let positions = [];
+    let indices = []
+    let textureCoordinates = [];
     
     const rotAngle = 2 *  Math.PI / sides; // in radians
     const rotAngleLat = (Math.PI - 2 * EarthDelta) / sides; // in radians
-    
+    const step = 1.0 / sides;
     for(var i = 0; i < sides; i++) {
         for (var j = 0; j < sides; j++) {
             // geo latitude = 90 - lat
@@ -149,6 +165,13 @@ function initBuffers(gl) {
                 Math.sin(lonr) * Math.sin(latt), Math.cos(latt), Math.cos(lonr) * Math.sin(latt),
                 Math.sin(lonr) * Math.sin(latb), Math.cos(latb), Math.cos(lonr) * Math.sin(latb),
             ]);
+            textureCoordinates = textureCoordinates.concat([
+                // 0, 0, step * i, 0, step * i, step * j, 0, step * j,
+                step * i, step * j, step * i, step * (j + 1), 
+                step * (i + 1), step * j, step * (i + 1), step * (j + 1), 
+                
+                
+            ]);
             indices = indices.concat([ind, ind + 1, ind + 2, ind + 2, ind + 1, ind + 3]);
             ind += 4;
             vertCount += 6;
@@ -157,47 +180,52 @@ function initBuffers(gl) {
             colors = colors.concat(c, c, c, c);
         }
     }
-    // TOP and bottom
-    for (var j = -1; j <= 1; j += 2) {
-        for (var i = 0; i < sides; i++) {
-            var lonl = i * rotAngle;
-            var lonr = (i + 1) * rotAngle;
-            positions = positions.concat([
-                0, j * h, 0,
-                Math.sin(lonl) * Math.sin(EarthDelta), j * Math.cos(EarthDelta), Math.cos(lonl) * Math.sin(EarthDelta),
-                Math.sin(lonr) * Math.sin(EarthDelta), j * Math.cos(EarthDelta), Math.cos(lonr) * Math.sin(EarthDelta)
-            ]);
-            indices = indices.concat([ind, ind + 1, ind + 2]);
-            ind += 3;
-            // Repeat each color 3 times for the four vertices of the face
-            const c = [0.9, 0.9, 0.9, 0.9];
-            colors = colors.concat(c, c, c);
-            vertCount += 3;
-        }
-    }
+    // TOP and bottom TODO
+    // for (var j = -1; j <= 1; j += 2) {
+    //     for (var i = 0; i < sides; i++) {
+    //         var lonl = i * rotAngle;
+    //         var lonr = (i + 1) * rotAngle;
+    //         positions = positions.concat([0, j * h, 0,
+    //             Math.sin(lonl) * Math.sin(EarthDelta), j * Math.cos(EarthDelta), Math.cos(lonl) * Math.sin(EarthDelta),
+    //             Math.sin(lonr) * Math.sin(EarthDelta), j * Math.cos(EarthDelta), Math.cos(lonr) * Math.sin(EarthDelta)
+    //         ]);
+    //         indices = indices.concat([ind, ind + 1, ind + 2]);
+    //         ind += 3;
+    //         // Repeat each color 3 times for the four vertices of the face
+    //         const c = [0.9, 0.9, 0.9, 0.9];
+    //         colors = colors.concat(c, c, c);
+    //         vertCount += 3;
+    //     }
+    // }
     
     // Now pass the list of positions into WebGL to build the
     // shape. We do this by creating a Float32Array from the
     // JavaScript array, then use it to fill the current buffer.
+
+    // Create a buffer for the cube's vertex positions.
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
     const colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    const textureCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
 
     // Build the element array buffer; this specifies the indices
     // into the vertex arrays for each face's vertices.
     const indexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
     // Now send the element array to GL
-    gl.bufferData(
-        gl.ELEMENT_ARRAY_BUFFER,
-        new Uint16Array(indices),
-        gl.STATIC_DRAW
-    );
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+    
 
     return {
         verticesCount: vertCount,
+        textureCoord: textureCoordBuffer,
         position: positionBuffer,
         color: colorBuffer,
         indices: indexBuffer,
@@ -207,7 +235,7 @@ function initBuffers(gl) {
 //
 // Draw the scene.
 //
-function drawScene(gl, programInfo, buffers, deltaTime) {
+function drawScene(gl, programInfo, buffers, deltaTime, texture) {
     gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
     gl.clearDepth(1.0); // Clear everything
     gl.enable(gl.DEPTH_TEST); // Enable depth testing
@@ -226,7 +254,7 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
     // note: glmatrix.js always has the first argument
     // as the destination to receive the result.
    // mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
-    const eye = vec3.fromValues(0, 0, -15 + Math.sin(CONFIG.eyeDist) * 4);
+    const eye = vec3.fromValues(0, 0, CONFIG.eyePosition + Math.sin(CONFIG.eyeDist) * 2);
     CONFIG.eyeDist += deltaTime / (15 / Math.PI) ;
     const lookAt = vec3.fromValues(0, 0, 0);
     var lookAtMatrix = mat4.create();
@@ -306,6 +334,27 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
         );
         gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
     }
+
+    // Tell WebGL how to pull out the texture coordinates from
+    // the texture coordinate buffer into the textureCoord attribute.
+    {
+        const numComponents = 2;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoord);
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.textureCoord,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset
+        );
+        gl.enableVertexAttribArray(programInfo.attribLocations.textureCoord);
+    }
+
     // Tell WebGL which indices to use to index the vertices
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
     // Tell WebGL to use our program when drawing
@@ -321,6 +370,15 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
         false,
         modelViewMatrix
     );
+
+    // Specify the textures
+    // Tell WebGL we want to affect texture unit 0
+    gl.activeTexture(gl.TEXTURE0);
+    // // Bind the texture to texture unit 0
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    // Tell the shader we bound the texture to texture unit 0
+    gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
+
     {
         const type = gl.UNSIGNED_SHORT;
         const offset = 0;
@@ -384,6 +442,48 @@ function loadShader(gl, type, source) {
     return shader;
 }
 
+function loadTexture(gl, url, zoom) {
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    // Because images have to be download over the internet
+    // they might take a moment until they are ready.
+    // Until then put a single pixel in the texture so we can
+    // use it immediately. When the image has finished downloading
+    // we'll update the texture with the contents of the image.
+    const level = 0;
+    const internalFormat = gl.RGBA;
+    const width = 1;
+    const height = 1;
+    const border = 0;
+    const srcFormat = gl.RGBA;
+    const srcType = gl.UNSIGNED_BYTE;
+    const pixel = new Uint8Array([0, 0, 255, 255]); // opaque blue
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, width, height, border, srcFormat, srcType,pixel);
+
+    const image = new Image();
+    image.onload = function () {
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, srcFormat, srcType, image);
+        // WebGL1 has different requirements for power of 2 images
+        // vs non power of 2 images so check if the image is a  power of 2 in both dimensions.
+        gl.generateMipmap(gl.TEXTURE_2D);
+        // No, it's not a power of 2. Turn off mips and set wrapping to clamp to edge
+        // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+    };
+    image.crossOrigin = "anonymous";
+    image.src = url + "/0/0/0.png";
+    return texture;
+}
+
+function isPowerOf2(value) {
+    return (value & (value - 1)) == 0;
+}
+
+
 function registerSlider(idParam, uiPrefix, idInput, idLabel, updateBuffer) {
     var slider = document.getElementById(idInput);
     var sliderText = document.getElementById(idLabel);
@@ -408,5 +508,6 @@ function addListeners() {
     registerSlider('zoom', 'Z:', 'sliderZoom', 'sliderZoomText', true);
     registerSlider('zNear', 'zNear:', 'sliderZNear', 'sliderZNearText');
     registerSlider('zFar', 'zFar:', 'sliderZFar', 'sliderZFarText');
+    registerSlider('eyePosition', 'EYE:', 'sliderEyePos', 'sliderEyePosText');
     
 }
